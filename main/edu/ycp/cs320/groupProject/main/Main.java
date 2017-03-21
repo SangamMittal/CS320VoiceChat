310d3f3801aad7985accf801346cad7a0fb4cd28
ratha <ratha@192.168.168.103>
2017-03-21 16:15:30 -0400

    hi

--- main/edu/ycp/cs320/groupProject/main/Main.java
+++ main/edu/ycp/cs320/groupProject/main/Main.java
@@ -10,7 +10,7 @@ public class Main {
 	public static void main(String[] args) throws Exception {
 		Server server = new Server(8081);
 		
-		
+		// This is Rathana's comment
 		//Josh's comment after switching project file
 
 		// Create and register a webapp context
