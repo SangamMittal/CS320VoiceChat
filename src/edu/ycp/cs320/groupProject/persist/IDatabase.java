fe64ba0879aa15827aa0c9908cdaa6c8bd6a7651
Joshua Amoros <jamoros@ycp.edu>
2017-04-12 13:41:01 -0400

    divvy up the IDatabase work

--- src/edu/ycp/cs320/groupProject/persist/IDatabase.java
+++ src/edu/ycp/cs320/groupProject/persist/IDatabase.java
@@ -2,14 +2,44 @@ package edu.ycp.cs320.groupProject.persist;
 
 import java.util.List;
 
+import edu.ycp.cs320.groupProject.model.Chatroom;
+import edu.ycp.cs320.groupProject.model.Post;
 import edu.ycp.cs320.groupProject.model.User;
 
 public interface IDatabase {
 
-
+//done
 	public Boolean signUp(User u);
-
+//done
 	public boolean Login(User u);
+	
+//josh	
+	public Boolean deleteUser(User u);
+	//josh
+	public Boolean deleteChatroom(Chatroom c);
+	//josh
+	public Boolean createChatroom(Chatroom c, User u);
+	//josh
+	public List<Post> selectMessages(Chatroom c);
+	//rathana
+	public Boolean removeUserFromChatroom(Chatroom c, User u);
+	//rathana
+	public User selectAdminFromChatroom(Chatroom c);
+	//rathana
+	public Boolean changeAdmin(Chatroom c, User u);
+	//brad
+	public Boolean insertMessages(Chatroom c, Post p);
+	//brad
+	public List<Chatroom> selectAllChatrooms();
+	//brad
+	public Boolean insertUserIntoChatroom(User u, Chatroom c);
+	
+	
+	
+	
+	
+	
+	
 
 	
 	
